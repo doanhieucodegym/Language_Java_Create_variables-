5a9675bc7d3d0c246bb8cd30cd45df69a852af00
Doan Van Hieu <doanhieu.bl.hn@gmail.com>
2019-09-30 22:22:37 +0700

    Variable

---
+++ src/Variables_ja.java
@@ -0,0 +1,15 @@
+public class Variables_ja {
+    public static void main(String[] args) {
+        int  i = 10;
+        float f =20.5f;
+        double d = 20;
+        char c ='a';
+        String s ="Đoàn Hiệu";
+        System.out.println("i ="+i);
+        System.out.println("f ="+f);
+        System.out.println("d ="+d);
+        System.out.println("c ="+c);
+        System.out.println("s ="+s);
+
+    }
+}
